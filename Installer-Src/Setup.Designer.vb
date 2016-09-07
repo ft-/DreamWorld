6d37a6a71911ac24f57c053798503f740164e792
Fred Beckhusen <fred@mitsi.com>
2016-09-07 00:33:58 -0500

    OnLook UI changes first look

--- Installer-Src/Setup.Designer.vb
+++ Installer-Src/Setup.Designer.vb
@@ -23,25 +23,31 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.StartButton = New System.Windows.Forms.Button()
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
         Me.Label = New System.Windows.Forms.Label()
         Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
-        Me.StopButton = New System.Windows.Forms.Button()
+        Me.WebBrowser3 = New System.Windows.Forms.WebBrowser()
         Me.BusyButton = New System.Windows.Forms.Button()
-        Me.Console = New System.Windows.Forms.CheckBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.StartButton = New System.Windows.Forms.Button()
         Me.InstallButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsoleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuShow = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHide = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EasyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuEasy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuFull = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AvatarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuVisible = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuInvisible = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuLogin = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAbout = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'StartButton
-        '
-        Me.StartButton.Location = New System.Drawing.Point(288, 14)
-        Me.StartButton.Name = "StartButton"
-        Me.StartButton.Size = New System.Drawing.Size(75, 23)
-        Me.StartButton.TabIndex = 1
-        Me.StartButton.Text = "Start"
-        Me.StartButton.UseVisualStyleBackColor = True
         '
         'WebBrowser1
         '
@@ -56,7 +62,7 @@ Partial Class Form1
         Me.Label.AutoSize = True
         Me.Label.BackColor = System.Drawing.Color.Black
         Me.Label.ForeColor = System.Drawing.Color.White
-        Me.Label.Location = New System.Drawing.Point(33, 19)
+        Me.Label.Location = New System.Drawing.Point(12, 279)
         Me.Label.Name = "Label"
         Me.Label.Size = New System.Drawing.Size(0, 13)
         Me.Label.TabIndex = 6
@@ -69,73 +75,183 @@ Partial Class Form1
         Me.WebBrowser2.Size = New System.Drawing.Size(125, 53)
         Me.WebBrowser2.TabIndex = 8
         '
-        'StopButton
+        'WebBrowser3
         '
-        Me.StopButton.Location = New System.Drawing.Point(288, 14)
-        Me.StopButton.Name = "StopButton"
-        Me.StopButton.Size = New System.Drawing.Size(75, 23)
-        Me.StopButton.TabIndex = 10
-        Me.StopButton.Text = "Stop"
-        Me.StopButton.UseVisualStyleBackColor = True
+        Me.WebBrowser3.Location = New System.Drawing.Point(2, 57)
+        Me.WebBrowser3.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser3.Name = "WebBrowser3"
+        Me.WebBrowser3.ScriptErrorsSuppressed = True
+        Me.WebBrowser3.ScrollBarsEnabled = False
+        Me.WebBrowser3.Size = New System.Drawing.Size(400, 270)
+        Me.WebBrowser3.TabIndex = 14
+        Me.WebBrowser3.Url = New System.Uri("http://www.outworldz.com/DreamWorld/Home.htm", System.UriKind.Absolute)
         '
         'BusyButton
         '
-        Me.BusyButton.Location = New System.Drawing.Point(288, 14)
+        Me.BusyButton.Location = New System.Drawing.Point(201, 28)
         Me.BusyButton.Name = "BusyButton"
         Me.BusyButton.Size = New System.Drawing.Size(75, 23)
-        Me.BusyButton.TabIndex = 11
+        Me.BusyButton.TabIndex = 18
         Me.BusyButton.Text = "Busy"
         Me.BusyButton.UseVisualStyleBackColor = True
         '
-        'Console
+        'StopButton
         '
-        Me.Console.AutoSize = True
-        Me.Console.Location = New System.Drawing.Point(370, 294)
-        Me.Console.Name = "Console"
-        Me.Console.Size = New System.Drawing.Size(81, 17)
-        Me.Console.TabIndex = 12
-        Me.Console.Text = "CheckBox1"
-        Me.Console.UseVisualStyleBackColor = True
+        Me.StopButton.Location = New System.Drawing.Point(303, 28)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(75, 23)
+        Me.StopButton.TabIndex = 17
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
         '
-        'Label1
+        'StartButton
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Black
-        Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(285, 295)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(80, 13)
-        Me.Label1.TabIndex = 13
-        Me.Label1.Text = "Show Consoles"
+        Me.StartButton.Location = New System.Drawing.Point(105, 28)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 16
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
         '
         'InstallButton
         '
-        Me.InstallButton.Location = New System.Drawing.Point(288, 14)
+        Me.InstallButton.Location = New System.Drawing.Point(24, 27)
         Me.InstallButton.Name = "InstallButton"
         Me.InstallButton.Size = New System.Drawing.Size(75, 23)
-        Me.InstallButton.TabIndex = 0
+        Me.InstallButton.TabIndex = 15
         Me.InstallButton.Text = "Install"
         Me.InstallButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ConsoleToolStripMenuItem, Me.EasyToolStripMenuItem, Me.AvatarToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(402, 24)
+        Me.MenuStrip1.TabIndex = 21
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(92, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'ConsoleToolStripMenuItem
+        '
+        Me.ConsoleToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuShow, Me.mnuHide})
+        Me.ConsoleToolStripMenuItem.Name = "ConsoleToolStripMenuItem"
+        Me.ConsoleToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.ConsoleToolStripMenuItem.Text = "Console"
+        '
+        'mnuShow
+        '
+        Me.mnuShow.Name = "mnuShow"
+        Me.mnuShow.Size = New System.Drawing.Size(103, 22)
+        Me.mnuShow.Text = "Show"
+        '
+        'mnuHide
+        '
+        Me.mnuHide.Checked = True
+        Me.mnuHide.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.mnuHide.Name = "mnuHide"
+        Me.mnuHide.Size = New System.Drawing.Size(103, 22)
+        Me.mnuHide.Text = "Hide"
+        '
+        'EasyToolStripMenuItem
+        '
+        Me.EasyToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.EasyToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuEasy, Me.mnuFull})
+        Me.EasyToolStripMenuItem.Name = "EasyToolStripMenuItem"
+        Me.EasyToolStripMenuItem.Size = New System.Drawing.Size(30, 20)
+        Me.EasyToolStripMenuItem.Text = "UI"
+        '
+        'mnuEasy
+        '
+        Me.mnuEasy.Checked = True
+        Me.mnuEasy.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.mnuEasy.Name = "mnuEasy"
+        Me.mnuEasy.Size = New System.Drawing.Size(107, 22)
+        Me.mnuEasy.Text = "Easy"
+        '
+        'mnuFull
+        '
+        Me.mnuFull.Name = "mnuFull"
+        Me.mnuFull.Size = New System.Drawing.Size(107, 22)
+        Me.mnuFull.Text = "Full UI"
+        '
+        'AvatarToolStripMenuItem
+        '
+        Me.AvatarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuVisible, Me.mnuInvisible})
+        Me.AvatarToolStripMenuItem.Name = "AvatarToolStripMenuItem"
+        Me.AvatarToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AvatarToolStripMenuItem.Text = "Avatar"
+        '
+        'mnuVisible
+        '
+        Me.mnuVisible.Name = "mnuVisible"
+        Me.mnuVisible.Size = New System.Drawing.Size(117, 22)
+        Me.mnuVisible.Text = "Visible"
+        '
+        'mnuInvisible
+        '
+        Me.mnuInvisible.Checked = True
+        Me.mnuInvisible.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.mnuInvisible.Name = "mnuInvisible"
+        Me.mnuInvisible.Size = New System.Drawing.Size(117, 22)
+        Me.mnuInvisible.Text = "Invisible"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuLogin, Me.mnuAbout})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'mnuLogin
+        '
+        Me.mnuLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.mnuLogin.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.mnuLogin.Name = "mnuLogin"
+        Me.mnuLogin.Size = New System.Drawing.Size(107, 22)
+        Me.mnuLogin.Text = "Login"
+        '
+        'mnuAbout
+        '
+        Me.mnuAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.mnuAbout.Name = "mnuAbout"
+        Me.mnuAbout.Size = New System.Drawing.Size(107, 22)
+        Me.mnuAbout.Text = "About"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(409, 323)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Console)
+        Me.ClientSize = New System.Drawing.Size(402, 328)
+        Me.Controls.Add(Me.Label)
         Me.Controls.Add(Me.BusyButton)
         Me.Controls.Add(Me.StopButton)
-        Me.Controls.Add(Me.WebBrowser2)
-        Me.Controls.Add(Me.Label)
-        Me.Controls.Add(Me.WebBrowser1)
         Me.Controls.Add(Me.StartButton)
         Me.Controls.Add(Me.InstallButton)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.WebBrowser3)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Setup DreamWorldFiles"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -149,13 +265,27 @@ Partial Class Form1
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
-    Friend WithEvents StartButton As System.Windows.Forms.Button
     Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
     Friend WithEvents Label As System.Windows.Forms.Label
     Friend WithEvents WebBrowser2 As System.Windows.Forms.WebBrowser
-    Friend WithEvents StopButton As System.Windows.Forms.Button
+    Friend WithEvents WebBrowser3 As System.Windows.Forms.WebBrowser
     Friend WithEvents BusyButton As System.Windows.Forms.Button
-    Friend WithEvents Console As System.Windows.Forms.CheckBox
-    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents StopButton As System.Windows.Forms.Button
+    Friend WithEvents StartButton As System.Windows.Forms.Button
     Friend WithEvents InstallButton As System.Windows.Forms.Button
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents EasyToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuEasy As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuFull As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ConsoleToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuShow As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuHide As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuLogin As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuAbout As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AvatarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuVisible As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuInvisible As System.Windows.Forms.ToolStripMenuItem
 End Class
--- Installer-Src/Setup.vb
+++ Installer-Src/Setup.vb
@@ -23,6 +23,7 @@ Public Class Form1
     Dim Opensim As Process
     Dim Running As Boolean
 
+
     Private Sub Form1_Leave(sender As Object, e As System.EventArgs) Handles Me.Leave
         ' Needed for some systems to clean up the stack, better be safe
         ZapAll()
@@ -40,7 +41,7 @@ Public Class Form1
         ' for debugging when compiling
        
         ' FKB debug only
-        ' CurDir = "C:\DreamWorld"
+        'CurDir = "C:\DreamWorld"
         ' ChDir(CurDir)
 
         Me.Text = "Opensimulator DreamWorld"
@@ -61,94 +62,6 @@ Public Class Form1
         Application.DoEvents()
     End Sub
 
-    Private Sub Start_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartButton.Click
-
-        ' Start Mowes, which starts MySql and Apache automatically.
-        Buttons(BusyButton)
-        Running = True
-
-        Try
-            My.Computer.FileSystem.DeleteFile(CurDir + "\DreamWorldFiles\Opensim\bin\Opensim.log")
-        Catch ex As Exception
-            ' do nothing
-        End Try
-
-        Try
-            My.Computer.FileSystem.DeleteFile(CurDir + "\DreamWorldFiles\Opensim\bin\OpenSimConsoleHistory.txt")
-        Catch ex As Exception
-            ' do nothing
-        End Try
-
-        Label.Visible = True
-        Print("Starting Mowes")
-
-        Dim p As Process = New Process()
-        Dim pi As ProcessStartInfo = New ProcessStartInfo()
-        pi.Arguments = ""
-
-        If Not Console.Checked Then
-            pi.WindowStyle = ProcessWindowStyle.Minimized
-        End If
-        pi.FileName = CurDir & "\DreamWorldFiles\Mowes.exe"
-        p.StartInfo = pi
-        p.Start()
-
-        Dim iSRunning As Integer
-        iSRunning = 30000
-        Dim Status = 0
-
-        ' wait for Mowes to come up on port 62535
-        While iSRunning > 0
-            Application.DoEvents()
-            Sleep(1000)
-            iSRunning = iSRunning - 1
-            If iSRunning = 0 Then
-                MsgBox("Timeout running MySQL - cannot continue", vbAbort)
-                ZapAll()
-                Buttons(StopButton)
-                Return
-            End If
-
-            ' now check that SQL server has started
-            Status = CheckMySQL()
-            If Status Then
-                iSRunning = 0
-                Print("MySql is Up")
-            End If
-        End While
-        ctr = 0 ' retry counter reset - lets give them a minute to get on
-        WebBrowser1.Navigate("http://127.0.0.1:62535/start/up.htm")
-        Application.DoEvents()
-    End Sub
-
-    Private Sub Install_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles InstallButton.Click
-
-        Buttons(BusyButton)
-
-        Print("Installing Shortcut")
-        Create_ShortCut(CurDir & "\Start.exe")
-        Print("Installing Onlook Viewer")
-
-        Dim p As Process = New Process()
-        Dim pi As ProcessStartInfo = New ProcessStartInfo()
-        pi.Arguments = ""
-        pi.FileName = CurDir & "\Viewer\Onlook.exe"
-        p.StartInfo = pi
-        p.Start()
-
-        Print("Installing Grid Info")
-
-        Dim appData As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData
-
-        Dim path As String
-        path = Mid(appData, 1, InStr(appData, "AppData") - 1)
-
-        My.Computer.FileSystem.CopyFile(CurDir & "\Viewer\grids_sg1.xml", path + "\AppData\Roaming\OnLook\user_settings\grids_sg1.xml", True)
-
-        ' allow them to launch now
-        Print("Ready to Launch")
-        Buttons(StartButton)
-    End Sub
 
     Private Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
 
@@ -174,7 +87,7 @@ Public Class Form1
 
             ' http://opensimulator.org/wiki/OpenSim.exe_Command_Line_Options
 
-            If Not Console.Checked Then
+            If mnuHide.Visible Then
                 pi.Arguments = "-console rest -background True "
                 pi.WindowStyle = ProcessWindowStyle.Hidden
             End If
@@ -232,15 +145,7 @@ Public Class Form1
 
     End Sub
 
-    Private Sub StopButton_Click(sender As System.Object, e As System.EventArgs) Handles StopButton.Click
 
-        Buttons(BusyButton)
-        Print("Stopping")
-        ZapAll()
-        Buttons(StartButton)
-        Print("")
-
-    End Sub
 
     Private Function CheckMySQL() As Boolean
 
@@ -272,7 +177,7 @@ Public Class Form1
     End Function
 
 
-    Private Sub Busy_Click(sender As System.Object, e As System.EventArgs) Handles BusyButton.Click
+    Private Sub Busy_Click(sender As System.Object, e As System.EventArgs)
 
         ' Busy click shows we are busy
 
@@ -330,6 +235,183 @@ Public Class Form1
         Label.Text = Value
         Application.DoEvents()
         Sleep(100)  ' time to read
+    End Sub
+
+    Private Sub HideToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles mnuHide.Click
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Hide.htm")
+        mnuShow.Checked = False
+        mnuHide.Checked = True
+
+        If Running Then
+            MsgBox("Change will occur when the Viewer is next logged in", vbInformation)
+        End If
+
+    End Sub
+
+    Private Sub mnuShow_Click(sender As System.Object, e As System.EventArgs) Handles mnuShow.Click
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Show.htm")
+        mnuShow.Checked = True
+        mnuHide.Checked = False
+
+        If Running Then
+            MsgBox("Change will occur when the Viewer is next logged in", vbInformation)
+        End If
+    End Sub
+
+    Private Sub mnuEasy_Click(sender As System.Object, e As System.EventArgs) Handles mnuEasy.Click
+        My.Computer.FileSystem.CopyFile(CurDir & "\DreamWorldFiles\Opensim\bin\ViewerSupport\panel_no_toolbar.xml.example", CurDir & "\DreamWorldFiles\Opensim\bin\ViewerSupport\panel_toolbar.xml", True)
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Easy.htm")
+        mnuEasy.Checked = True
+        mnuFull.Checked = False
+
+        If Running Then
+            MsgBox("Change will occur when the sim is restarted", vbInformation)
+        End If
+    End Sub
+
+    Private Sub FmnuFull_Click(sender As System.Object, e As System.EventArgs) Handles mnuFull.Click
+        My.Computer.FileSystem.CopyFile(CurDir & "\DreamWorldFiles\Opensim\bin\ViewerSupport\panel_toolbar.xml.example", CurDir & "\DreamWorldFiles\Opensim\bin\ViewerSupport\panel_toolbar.xml", True)
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Full.htm")
+        mnuEasy.Checked = False
+        mnuFull.Checked = True
+
+        If Running Then
+            MsgBox("Change will occur when the sim is restarted", vbInformation)
+        End If
+    End Sub
+
+    Private Sub VisibleToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles mnuVisible.Click
+        My.Computer.FileSystem.CopyFile(CurDir & "\DreamWorldFiles\Opensim\bin\config-include\Camera.ini.example", CurDir & "\DreamWorldFiles\Opensim\bin\config-include\Camera.ini", True)
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Visible.htm")
+        mnuVisible.Checked = True
+        mnuInvisible.Checked = False
+
+        If Running Then
+            MsgBox("Change will occur when the Viewer is next logged in", vbInformation)
+        End If
+    End Sub
+
+    Private Sub InvisibleToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles mnuInvisible.Click
+        My.Computer.FileSystem.CopyFile(CurDir & "\DreamWorldFiles\Opensim\bin\config-include\NoCamera.ini.example", CurDir & "\DreamWorldFiles\Opensim\bin\config-include\Camera.ini", True)
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Invisible.htm")
+        mnuVisible.Checked = False
+        mnuInvisible.Checked = True
+
+        If Running Then
+            MsgBox("Change will occur when the Viewer is next logged in", vbInformation)
+        End If
+    End Sub
+
+    Private Sub mnuExit_Click(sender As System.Object, e As System.EventArgs) Handles mnuExit.Click
+        Print("Stopping")
+        Application.DoEvents()
+        ZapAll()
+        Buttons(StartButton)
+        Print("")
+        End
+    End Sub
+
+    Private Sub mnuLogin_Click(sender As System.Object, e As System.EventArgs) Handles mnuLogin.Click
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/Help.htm")
+    End Sub
+
+    Private Sub mnuAbout_Click(sender As System.Object, e As System.EventArgs) Handles mnuAbout.Click
+        WebBrowser3.Url = New Uri("http://www.Outworldz.com/DreamWorld/About.htm")
+    End Sub
+
+    Private Sub StartButton_Click(sender As System.Object, e As System.EventArgs) Handles StartButton.Click
+        ' Start Mowes, which starts MySql and Apache automatically.
+        Buttons(BusyButton)
+        Running = True
+
+        Try
+            My.Computer.FileSystem.DeleteFile(CurDir + "\DreamWorldFiles\Opensim\bin\Opensim.log")
+        Catch ex As Exception
+            ' do nothing
+        End Try
+
+        Try
+            My.Computer.FileSystem.DeleteFile(CurDir + "\DreamWorldFiles\Opensim\bin\OpenSimConsoleHistory.txt")
+        Catch ex As Exception
+            ' do nothing
+        End Try
+
+        Label.Visible = True
+        Print("Starting Mowes")
+
+        Dim p As Process = New Process()
+        Dim pi As ProcessStartInfo = New ProcessStartInfo()
+        pi.Arguments = ""
+
+        If mnuHide.Visible Then
+            pi.WindowStyle = ProcessWindowStyle.Minimized
+        End If
+        pi.FileName = CurDir & "\DreamWorldFiles\Mowes.exe"
+        p.StartInfo = pi
+        p.Start()
+
+        Dim iSRunning As Integer
+        iSRunning = 30000
+        Dim Status = 0
+
+        ' wait for Mowes to come up on port 62535
+        While iSRunning > 0
+            Application.DoEvents()
+            Sleep(1000)
+            iSRunning = iSRunning - 1
+            If iSRunning = 0 Then
+                MsgBox("Timeout running MySQL - cannot continue", vbAbort)
+                ZapAll()
+                Buttons(StopButton)
+                Return
+            End If
+
+            ' now check that SQL server has started
+            Status = CheckMySQL()
+            If Status Then
+                iSRunning = 0
+                Print("MySql is Up")
+            End If
+        End While
+        ctr = 0 ' retry counter reset - lets give them a minute to get on
+        WebBrowser1.Navigate("http://127.0.0.1:62535/start/up.htm")
+        Application.DoEvents()
+    End Sub
+
+    Private Sub StopButton_Click_1(sender As System.Object, e As System.EventArgs) Handles StopButton.Click
+        Buttons(BusyButton)
+        Print("Stopping")
+        ZapAll()
+        Buttons(StartButton)
+        Print("")
+    End Sub
+
+    Private Sub InstallButton_Click(sender As System.Object, e As System.EventArgs) Handles InstallButton.Click
+        Buttons(BusyButton)
+
+        Print("Installing Shortcut")
+        Create_ShortCut(CurDir & "\Start.exe")
+        Print("Installing Onlook Viewer")
+
+        Dim p As Process = New Process()
+        Dim pi As ProcessStartInfo = New ProcessStartInfo()
+        pi.Arguments = ""
+        pi.FileName = CurDir & "\Viewer\Onlook.exe"
+        p.StartInfo = pi
+        p.Start()
+
+        Print("Installing Grid Info")
+
+        Dim appData As String = My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData
+
+        Dim path As String
+        path = Mid(appData, 1, InStr(appData, "AppData") - 1)
+
+        My.Computer.FileSystem.CopyFile(CurDir & "\Viewer\grids_sg1.xml", path + "\AppData\Roaming\OnLook\user_settings\grids_sg1.xml", True)
+
+        ' allow them to launch now
+        Print("Ready to Launch")
+        Buttons(StartButton)
     End Sub
 End Class
 
