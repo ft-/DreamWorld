d009d8d4a2a62db49c69104d50be06aed86ee338
Fred Beckhusen <fred@mitsi.com>
2017-10-30 21:00:17 -0500

    Remove Update_Notes

--- Installer_Src/Setup.vb
+++ Installer_Src/Setup.vb
@@ -37,11 +37,10 @@ Public Class Form1
 #Region "Declarations"
 
     Dim MyVersion As String = "1.8"
-    Dim DebugPath As String = "C:\Opensim\Outworldz-2.0"
+    Dim DebugPath As String = "C:\Opensim\Outworldz-2.0"  ' no slash at end
     Public Domain As String = "http://www.outworldz.com"
     Public prefix As String ' Holds path to bin folder
 
-    Dim RevNotesFile As String = "Update_Notes_" + MyVersion + ".rtf"
     Private gFailDebug1 = False ' set to true to fail diagnostic
     Private gFailDebug2 = False ' set to true to fail diagnostic
     Private gFailDebug3 = False ' set to true to fail diagnostic
@@ -210,9 +209,7 @@ Public Class Form1
 
         MyUPnPMap = New UPNP(MyFolder)
 
-        If System.IO.File.Exists(MyFolder + "\" + RevNotesFile) Then
-            System.Diagnostics.Process.Start("wordpad.exe", """" + MyFolder + "\" + RevNotesFile + """")
-        End If
+
 
         RegionClass = New RegionMaker
 
@@ -394,15 +391,6 @@ Public Class Form1
 
         Dim p As Point
         p = Me.Location
-
-        If System.IO.File.Exists(MyFolder + "\" + RevNotesFile) Then
-            Try
-                My.Computer.FileSystem.RenameFile(MyFolder + "\" + RevNotesFile, "Update_Notes_for_Rev_" + MyVersion + ".rtf")
-            Catch ex As Exception
-                Log("Error: Failed to remove rev notes" + ex.Message)
-            End Try
-
-        End If
 
         My.Settings.MyX = p.X
         My.Settings.MyY = p.Y
