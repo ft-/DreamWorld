a9037ee49e16ca691c530fb18b1d60c2cf706365
Fred Beckhusen <fred@mitsi.com>
2017-12-08 18:40:32 -0600

    Perl Builder updated Revision,txt updated
    Signed-off-by: Fred Beckhusen <fred@mitsi.com>

--- Make_zip_v2.pl
+++ Make_zip_v2.pl
@@ -1,7 +1,5 @@
 
-my $type  = '-V2.0 Beta';# '-Beta-V1.5';
-
-my $rev = 'V2.0';
+my $type  = '-V2.0-Beta-1';# '-Beta-V1.5';
 my $dir = "C:\\Opensim\\Outworldz-Source";
 
 
@@ -10,26 +8,14 @@ use File::Copy;
 use File::Path;
 use 5.010;
 
-my @deletions = ("$dir/OutworldzFiles/addins-registry",
+my @deletions = (
 				 "$dir/OutworldzFiles/AutoBackup",
-				 
 				 "$dir/OutworldzFiles/Opensim/bin/assetcache",
 				 "$dir/OutworldzFiles/Opensim/bin/j2kDecodeCache",
 				 "$dir/OutworldzFiles/Opensim/bin/autobackup",
 				 "$dir/OutworldzFiles/Opensim/bin/ScriptEngines",
 				 "$dir/OutworldzFiles/Opensim/bin/maptiles",
-				 "$dir/OutworldzFiles/Opensim/bin/addin-db-002",
 				 "$dir/OutworldzFiles/Opensim/bin/bakes",
-				 
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/assetcache",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/j2kDecodeCache",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/autobackup",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/ScriptEngines",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/maptiles",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/addin-db-002",
-				 "$dir/OutworldzFiles/Opensim-0.9/bin/bakes",
-				 "$dir/OutworldzFiles/mysql/data/opensim",
-				 "$dir/OutworldzFiles/mysql/data/robust",
 				 );
 
 foreach my $path ( @deletions) {
@@ -38,24 +24,16 @@ foreach my $path ( @deletions) {
 
 #clean up opensim
 unlink "$dir/OutworldzFiles/Opensim/bin/Opensim.log" ;
-unlink "$dir/OutworldzFiles/Opensim-0.9/bin/Opensim.log" ;
-
 unlink "$dir/OutworldzFiles/Opensim/bin/Opensimstats.log" ;
-unlink "$dir/OutworldzFiles/Opensim-0.9/bin/Opensimstats.log" ;
-
 
 unlink "$dir/OutworldzFiles/Opensim/bin/OpensimConsoleHistory.txt" ;
 unlink "$dir/OutworldzFiles/Opensim-0.9/bin/OpensimConsoleHistory.txt" ;
 
 unlink "$dir/OutworldzFiles/Opensim/bin/LocalUserStatistics.db" ;
-unlink "$dir/OutworldzFiles/Opensim-0.9/bin/LocalUserStatistics.db" ;
 
 #mysql
 unlink "$dir/OutworldzFiles/mysql/data/Alienware.err" ;
 unlink "$dir/OutworldzFiles/mysql/data/Alienware.pid" ;
-unlink	"$dir/OutworldzFiles/mysql/data/ib_logfile0";
-unlink	"$dir/OutworldzFiles/mysql/data/ib_logfile1";
-unlink	"$dir/OutworldzFiles/mysql/data/ibdata1";
 
 #logs
 unlink "$dir/OutworldzFiles/Diagnostics.log" ;
@@ -64,11 +42,12 @@ unlink "$dir/OutworldzFiles/Init.txt" ;
 unlink "$dir/OutworldzFiles/upnp.log" ;
 
 
-unlink "../Zips/Outworldz$type.zip" ;
+unlink "../Zips/DreamGrid$type.zip" ;
 unlink "../Zips/Outworldz-Update$type.zip" ;
 
-print "Making binaries, please be sure they are signed\n";
 
+print "Making binaries, please be sure they are signed\n";
+<STDIN>;
 
 # SIGN FIRST
 
@@ -87,47 +66,47 @@ my @files =   glob("$dir\\*");
 
 foreach (@files) {
 	next if -d $_;
-	Process ("../7z.exe -tzip a ..\\Zips\\Outworldz$type.zip $_ ");
+	next if $_ eq 'Make_zip_v2.pl';
+	Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid$type.zip $_ ");
 }
 
 say("Adding folders");
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz-Update$type.zip Licenses_to_Content");
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz-Update$type.zip OutworldzFiles");
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz-Update$type.zip Viewer");
+Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid-Update$type.zip Licenses_to_Content");
+Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid-Update$type.zip OutworldzFiles");
+#Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid-Update$type.zip Viewer");
 
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz$type.zip Licenses_to_Content");
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz$type.zip OutworldzFiles");
-Process ("../7z.exe -tzip a ..\\Zips\\Outworldz$type.zip Viewer");
+Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid$type.zip Licenses_to_Content");
+Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid$type.zip OutworldzFiles");
+#Process ("../7z.exe -tzip a ..\\Zips\\DreamGrid$type.zip Viewer");
 
 		
 say("Updater Build");
-if (!copy ("../Zips/Outworldz$type.zip", "../Zips/Outworldz-Update$type.zip"))  {die $!;}
+if (!copy ("../Zips/DreamGrid$type.zip", "../Zips/DreamGrid-Update$type.zip"))  {die $!;}
 
-say("Drop mysql files");
+say("Drop mysql files from update");
 # now delete the mysql from the UPDATE
-Process ("../7z.exe -tzip d ..\\Zips\\Outworldz-Update$type.zip Outworldzfiles\\mysql\\data -r ");
+Process ("../7z.exe -tzip d ..\\Zips\\DreamGrid-Update$type.zip DreamGridfiles\\mysql\\data -r ");
 
 
-# Dot net because we cannot overwrite an open file
-Process ("../7z.exe -tzip d ..\\Zips\\Outworldz-Update$type.zip DotNetZip.dll ");
+# del Dot net because we cannot overwrite an open file
+Process ("../7z.exe -tzip d ..\\Zips\\DreamGrid-Update$type.zip DotNetZip.dll ");
 
-say("Remove Outworldz.ini");
-# and Outworldz.ini so we do not end up with both Outworldz and RegionConfig.ini
-Process ("../7z.exe -tzip d ..\\Zips\\Outworldz-Update$type.zip OutworldzFiles\\Opensim\\bin\\Regions\\Outworldz.ini");		
-Process ("../7z.exe -tzip d ..\\Zips\\Outworldz-Update$type.zip OutworldzFiles\\Opensim-0.9\\bin\\Regions\\Outworldz.ini");	
+say("Remove welcome region");
+# and WelcomeRegion.ini so we do not end up with both DreamGrid and RegionConfig.ini
+Process ("../7z.exe -tzip d ..\\Zips\\DreamGrid-Update$type.zip DreamGridFiles\\Opensim\\bin\\Regions\\WelcomeRegion");		
 
 
 #####################
 print "Server Copy\n";
 
 # Ready to move it all
-unlink "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/Outworldz$type.zip";
-if (!copy ("../Zips/Outworldz$type.zip", "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/Outworldz$type.zip"))  {die $!;}
+unlink "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/DreamGrid$type.zip";
+if (!copy ("../Zips/DreamGrid$type.zip", "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/DreamGrid$type.zip"))  {die $!;}
 
 #web server
 print "Server Copy Update\n";
-unlink "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/Outworldz-Update$type.zip";
-if (!copy ("../Zips/Outworldz-Update$type.zip", "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/Outworldz-Update$type.zip"))  {die $!;}
+unlink "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/DreamGrid-Update$type.zip";
+if (!copy ("../Zips/DreamGrid-Update$type.zip", "y:/Inetpub/Secondlife/Outworldz_Installer/Grid/DreamGrid-Update$type.zip"))  {die $!;}
 
 # lastly revisions file
 if (!copy ('Revisions.txt', 'y:/Inetpub/Secondlife/Outworldz_Installer/Revisions.txt'))  {die $!;}
@@ -165,5 +144,5 @@ my $path = shift;
 		  or ++$errors, warn("Can't remove $_: $!");
 	}
 	
-	exit(1) if $errors;
+	#exit(1) if $errors;
 }
