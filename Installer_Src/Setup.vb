4a98eb06946272e0b2766e6f832e8668e18b2fe4
Fred Beckhusen <fred@mitsi.com>
2018-01-05 10:02:02 -0600

    AGPL License for Dreamgrid

--- Installer_Src/Setup.vb
+++ Installer_Src/Setup.vb
@@ -1,22 +1,8 @@
 ﻿
 #Region "Copyright"
 ' Copyright 2014 Fred Beckhusen for Outworldz.com
-' https://opensource.org/licenses/MIT 
-'Permission Is hereby granted, free Of charge, to any person obtaining a copy of this software 
-' And associated documentation files (the "Software"), to deal in the Software without restriction, 
-'including without limitation the rights To use, copy, modify, merge, publish, distribute, sublicense,
-'And/Or sell copies Of the Software, And To permit persons To whom the Software Is furnished To 
-'Do so, subject To the following conditions:
+' https://opensource.org/licenses/AGPL
 
-'The above copyright notice And this permission notice shall be included In all copies Or '
-'substantial portions Of the Software.
-
-'THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or IMPLIED, 
-' INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY, FITNESS For A PARTICULAR 
-'PURPOSE And NONINFRINGEMENT.In NO Event SHALL THE AUTHORS Or COPYRIGHT HOLDERS BE LIABLE 
-'For ANY CLAIM, DAMAGES Or OTHER LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or 
-'OTHERWISE, ARISING FROM, OUT Of Or In CONNECTION With THE SOFTWARE Or THE USE Or OTHER 
-'DEALINGS IN THE SOFTWARE.Imports System
 
 #End Region
 
