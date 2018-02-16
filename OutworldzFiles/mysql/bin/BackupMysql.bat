690a1c3029484325ad0b0c03ba9f3121f3532566
Fred Beckhusen <fred@mitsi.com>
2018-02-16 16:30:39 -0600

    AutoBackup

--- OutworldzFiles/mysql/bin/BackupMysql.bat
+++ OutworldzFiles/mysql/bin/BackupMysql.bat
@@ -1,6 +1,14 @@
-@REM A program to backup Mysql manually
-mysqldump.exe --opt  -uroot --verbose opensim  > "C:/Opensim/Outworldz Source/OutworldzFiles/Autobackup/Opensim_2018-01-30_12_43_27.sql"
-mysqldump.exe --opt  -uroot --verbose robust  > "C:/Opensim/Outworldz Source/OutworldzFiles/Autobackup/Robust_2018-01-30_12_43_27.sql"
-@REM Finished!
-@pause
 
+@rem makes a full back into Autobackup of mysql, robust and opensim
+
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+mysqldump.exe --opt  -uroot --verbose mysql  > ..\..\Autobackup\mysql_%fullstamp%.sql
+mysqldump.exe --opt  -uroot --verbose opensim  > ..\..\Autobackup\Opensim_%fullstamp%.sql
+*mysqldump.exe --opt  -uroot --verbose robust  > ..\..\Autobackup\Robust_%fullstamp%.sql*
+xcopy /s /i ..\..\Opensim\bin\Regions ..\..\Autobackup\Regions_%fullstamp%
+@echo Finished!
