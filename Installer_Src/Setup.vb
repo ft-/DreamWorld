5037c3b35f26dbb4aa5f4a284a347823ea7e8c24
Fred Beckhusen <fred@mitsi.com>
2018-03-08 00:04:10 -0600

    V2.14 Zlib.dll

--- Installer_Src/Setup.vb
+++ Installer_Src/Setup.vb
@@ -35,7 +35,7 @@ Public Class Form1
 #Region "Declarations"
 
 
-    Dim MyVersion As String = "2.12"
+    Dim MyVersion As String = "2.14"
     Dim DebugPath As String = "C:\Opensim\Outworldz Source"  ' no slash at end
     Public Domain As String = "http://www.outworldz.com"
     Public prefix As String ' Holds path to Opensim folder
