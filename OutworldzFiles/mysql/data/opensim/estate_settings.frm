6297672ef0c9b19dd061a054d8201f16406d7332
Fred Beckhusen <fred@mitsi.com>
2018-05-29 12:07:46 -0500

    Remove Opensim Src

--- OutworldzFiles/mysql/data/opensim/estate_settings.frm
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c17667620fe78730935c319c31aa7ecd00ff16f6584310a0e21c9556d1f5cf5
-size 9870
--- OutworldzFiles/opensim/makebirds.bat
+++
@@ -1,9 +0,0 @@
-
-@rem commands to Compile and install Opensim Birds
-cd addon-modules
-git clone https://github.com/JakDaniels/OpenSimBirds
-cd ../
-call runprebuild.bat
-call compile.bat
-cd bin
-mautil -reg . reg-update
