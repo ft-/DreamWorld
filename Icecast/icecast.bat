8b3c2379c43aa812c77c9afc84dc6b9d73df1924
Fred Beckhusen <fred@mitsi.com>
2018-06-06 11:14:22 -0500

    icecast missing a log folder

--- Icecast/icecast.bat
+++ Icecast/icecast.bat
@@ -5,7 +5,7 @@ echo|set /p=Starting
 .\bin\icecast.exe -v
 echo Using config "icecast.xml" from installation directory ...
 echo.
-echo Please open http://localhost:8000 or http://127.0.0.1:8000 in your web browser to see the web interface.
+echo Please open http://localhost:8080 or http://127.0.0.1:8080 in your web browser to see the web interface.
 echo.
 echo.
 echo Leave this window open to keep Icecast running and, if necessary, minimize it.
