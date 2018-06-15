e642e8701ccf17a157b2b157cc9eb7412bd3c646
Fred Beckhusen <fred@mitsi.com>
2018-06-15 18:21:05 -0500

    Repair all 3 db's

--- OutworldzFiles/mysql/bin/Repair_ISAM.bat
+++ OutworldzFiles/mysql/bin/Repair_ISAM.bat
@@ -1,1 +1,3 @@
 myisamchk --force --fast --update-state ..\data\mysql\*.MYI
+myisamchk --force --fast --update-state ..\data\opensim\*.MYI
+myisamchk --force --fast --update-state ..\data\robust\*.MYI
