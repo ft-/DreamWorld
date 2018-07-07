0cbba7680a53dc022a3c867d7f5d82f61932cc36
Fred Beckhusen <fred@mitsi.com>
2018-07-07 00:12:43 -0500

    Opensim 0.9.0.1-Post-Fixes

--- Installer_Src/2010/SetupDreamWorld/Setup DreamWorld/My Project/Settings.Designer.vb
+++ Installer_Src/2010/SetupDreamWorld/Setup DreamWorld/My Project/Settings.Designer.vb
@@ -884,18 +884,6 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("DreamWorld")>  _
-        Public Property SimName() As String
-            Get
-                Return CType(Me("SimName"),String)
-            End Get
-            Set
-                Me("SimName") = value
-            End Set
-        End Property
-        
-        <Global.System.Configuration.UserScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.DefaultSettingValueAttribute("3309")>  _
         Public Property MySqlPort() As String
             Get
@@ -1023,6 +1011,18 @@ Namespace My
             End Get
             Set
                 Me("UPnPEnabled") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("DreamGrid")>  _
+        Public Property SimName() As String
+            Get
+                Return CType(Me("SimName"),String)
+            End Get
+            Set
+                Me("SimName") = value
             End Set
         End Property
     End Class
