dd5557c1bde13193d5a9b92de356c5ddf220f11c
Fred Beckhusen <fred@mitsi.com>
2018-07-07 19:16:12 -0500

    Clarify

--- OutworldzFiles/Icecast/icecast.bat
+++ OutworldzFiles/Icecast/icecast.bat
@@ -3,11 +3,9 @@ echo.
 echo.
 echo|set /p=Starting 
 .\bin\icecast.exe -v
-echo Using config "icecast.xml" from installation directory ...
-echo.
-echo Please open http://localhost:8080 or http://127.0.0.1:8080 in your web browser to see the web interface.
+echo Using config "icecast_run.xml" from Outworldzfiles\Icecast directory ...
 echo.
 echo.
-echo Leave this window open to keep Icecast running and, if necessary, minimize it.
+echo Leave this window open to keep Icecast running. You may minimize it.
 .\bin\icecast.exe -c .\icecast_run.xml
 
