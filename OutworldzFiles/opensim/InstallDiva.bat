1fe990344f6d92fdda905d362d9c40835348f3ff
Fred Beckhusen <fred@mitsi.com>
2018-07-18 13:30:32 -0500

    rm opensim

--- OutworldzFiles/opensim/InstallDiva.bat
+++
@@ -1,7 +0,0 @@
-cd bin
-mautil.exe -reg ./addins-registry reg-update
-mautil.exe -reg ./addins-registry rep-add http://metaverseink.com/repo
-mautil.exe -reg ./addins-registry reg-update
-mautil.exe -reg ./addins-registry  list-av
-@pause
-mautil.exe -reg ./addins-registry  install Diva.Wifi
