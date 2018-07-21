4493eccbefc82b6a728cef22646ea30c7444672c
Fred Beckhusen <fred@mitsi.com>
2018-07-21 06:32:41 -0500

    V.22

--- OutworldzFiles/mysql/bin/InstallAsAService.bat
+++ OutworldzFiles/mysql/bin/InstallAsAService.bat
@@ -1,2 +1,2 @@
 @REM Program to run Mysql as a Service
-mysqld.exe --install Mysql --defaults-file="E:/Opensim/Outworldz DreamGrid Source/OutworldzFiles/mysql/my.ini"
+mysqld.exe --install Mysql --defaults-file="/Opensim/Outworldz DreamGrid Source/OutworldzFiles/mysql/my.ini"
---
+++ OutworldzFiles/mysql/data/opensim/land.frm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3817af698798bcc1e2c59e33e95a461474ed9baac32a60f6164102f97f2f731e
+size 18624
