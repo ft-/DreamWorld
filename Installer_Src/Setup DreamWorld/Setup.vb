dfeef26dc158836d0e94a762da6ad1b62fc91573
Fred Beckhusen <fred@mitsi.com>
2018-12-19 10:24:42 -0600

    2.54 close down all regions in the Instance Clear caches with notice

--- Installer_Src/Setup DreamWorld/Setup.vb
+++ Installer_Src/Setup DreamWorld/Setup.vb
@@ -34,7 +34,7 @@ Public Class Form1
 
 #Region "Declarations"
 
-    Dim gMyVersion As String = "2.54"
+    Dim gMyVersion As String = "2.55"
     Dim gSimVersion As String = "0.9.1"
 
     ' edit this to compile and run in the correct folder root
@@ -555,7 +555,7 @@ Public Class Form1
         End If
 
         ProgressBar1.Value = 100
-        Application.DoEvents()
+
 
 
     End Sub
@@ -713,7 +713,7 @@ Public Class Form1
     End Sub
     Private Sub ShutdownNowToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs)
         Print("Stopping")
-        Application.DoEvents()
+
         KillAll()
         Buttons(StartButton)
         Print("Stopped")
@@ -749,7 +749,7 @@ Public Class Form1
         ProgressBar1.Visible = True
         ' close everything as gracefully as possible.
 
-        Application.DoEvents()
+
 
         StopIcecast()
 
@@ -807,7 +807,11 @@ Public Class Form1
                         If CheckPort("127.0.0.1", RegionClass.GroupPort(X)) Then
                             CountisRunning = CountisRunning + 1
                         Else
-                            RegionClass.ShuttingDown(X) = False
+                            ' close down all regions in ther Instance
+                            Dim gname = RegionClass.GroupName(X)
+                            For Each Y In RegionClass.RegionListByGroupNum(gname)
+                                RegionClass.ShuttingDown(Y) = False
+                            Next
                         End If
                     End If
                     Application.DoEvents()
@@ -842,7 +846,6 @@ Public Class Form1
 
         If gRobustProcID > 0 Then
             ConsoleCommand(gRobustProcID, "{ENTER}q{ENTER}")
-            Me.Focus()
         End If
 
         ' cannot load OAR or IAR, either
@@ -853,7 +856,7 @@ Public Class Form1
         Me.AllowDrop = False
 
         ProgressBar1.Value = 0
-        Application.DoEvents()
+
 
         gStopping = False
 
@@ -930,7 +933,7 @@ Public Class Form1
         TextBox1.Visible = True
         Application.DoEvents()
         Sleep(gChatTime)  ' time to read
-        Application.DoEvents()
+
 
     End Sub
     Public Sub PrintFast(Value As String)
@@ -939,8 +942,6 @@ Public Class Form1
         PictureBox1.Visible = False
         TextBox1.Visible = True
         TextBox1.Text = Value
-        Sleep(100)
-        Application.DoEvents()
 
     End Sub
 
@@ -1059,7 +1060,7 @@ Public Class Form1
                     Else
                         outputFile.WriteLine(line)
                     End If
-                    Application.DoEvents()
+
                 End While
             End Using
             'close your reader
@@ -1821,18 +1822,19 @@ Public Class Form1
 
         If Not OpensimIsRunning() Then
 
-            Dim folders() As String = IO.Directory.GetDirectories(gPath & "bin\ScriptEngines\")
-            Print("Clearing Script cache")
+            Dim folders() = IO.Directory.GetDirectories(gPath & "bin\ScriptEngines\")
+            Print("Clearing Script cache. This may take a long time!")
             For Each folder As String In folders
                 Dim scripts() As String = IO.Directory.GetFiles(folder)
-
+                Dim ctr As Integer = 0
                 For Each script As String In scripts
                     Dim ext = Path.GetExtension(script)
                     If ext <> ".state" Then
                         My.Computer.FileSystem.DeleteFile(script)
+                        ctr = ctr + 1
+                        PrintFast(ctr.ToString)
                         Application.DoEvents()
                     End If
-
                 Next
             Next
             Print("Clearing bakes")
@@ -1844,13 +1846,44 @@ Public Class Form1
 
         Print("Clearing Asset cache. This may take a long time!")
         Try
-            My.Computer.FileSystem.DeleteDirectory(gPath & "bin\assetcache\", FileIO.DeleteDirectoryOption.DeleteAllContents)
-            Print("Clearing Image cache")
-            My.Computer.FileSystem.DeleteDirectory(gPath & "bin\j2kDecodeCache\", FileIO.DeleteDirectoryOption.DeleteAllContents)
-            Print("Clearing Mesh cache")
-            My.Computer.FileSystem.DeleteDirectory(gPath & "bin\MeshCache\", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            Dim folders() = IO.Directory.GetDirectories(gPath & "bin\Assetcache\")
+            Print("Clearing Asset cache.")
+            Dim ctr As Integer = 0
+            For Each folder As String In Folders
+                My.Computer.FileSystem.DeleteDirectory(folder, FileIO.DeleteDirectoryOption.DeleteAllContents)
+                ctr = ctr + 1
+                PrintFast(ctr.ToString)
+                Application.DoEvents()
+            Next
         Catch
         End Try
+        Try
+            Print("Clearing Image cache")
+            Dim folders() = IO.Directory.GetDirectories(gPath & "bin\j2kDecodeCache\")
+            Dim ctr = 0
+            For Each folder As String In folders
+                My.Computer.FileSystem.DeleteDirectory(gPath & "bin\j2kDecodeCache\", FileIO.DeleteDirectoryOption.DeleteAllContents)
+                ctr = ctr + 1
+                PrintFast(ctr.ToString)
+                Application.DoEvents()
+            Next
+
+        Catch
+        End Try
+
+        Try
+            Print("Clearing Mesh cache")
+            Dim folders() = IO.Directory.GetDirectories(gPath & "bin\MeshCache\")
+            Dim ctr As Integer = 0
+            For Each folder As String In folders
+                My.Computer.FileSystem.DeleteDirectory(gPath & "bin\MeshCache\", FileIO.DeleteDirectoryOption.DeleteAllContents)
+                ctr = ctr + 1
+                PrintFast(ctr.ToString)
+                Application.DoEvents()
+            Next
+        Catch
+        End Try
+
 
         If Not OpensimIsRunning() Then
             Print("All Server Caches cleared")
@@ -3469,6 +3502,8 @@ Public Class Form1
             Log("Warn:" + ex.Message)
             Return False
         End Try
+        Me.Focus()
+        Application.DoEvents()
         Return True
 
     End Function
@@ -3668,7 +3703,7 @@ Public Class Form1
                 ' if its past time and no one is in the sim...
                 If timervalue >= MySetting.AutoRestartInterval() And Not AvatarsIsInGroup(Groupname) Then
                     ' shut down the group
-                    ConsoleCommand(RegionClass.ProcessID(X), "{ENTER}q{ENTER}")
+                    ConsoleCommand(RegionClass.ProcessID(X), "q{ENTER}q{ENTER}")
                     Print("AutoRestarting " + Groupname)
                     For Each RegionNum As Integer In RegionClass.RegionListByGroupNum(Groupname)
                         RegionClass.Booted(RegionNum) = False
@@ -3721,10 +3756,10 @@ Public Class Form1
     Private Sub BumpProgress(bump As Integer)
 
         Dim nextval As Integer = ProgressBar1.Value + bump
-            If nextval > 100 Then
-                nextval = 100
-            End If
-            ProgressBar1.Value = nextval
+        If nextval > 100 Then
+            nextval = 100
+        End If
+        ProgressBar1.Value = nextval
 
         Application.DoEvents()
     End Sub
@@ -3776,7 +3811,6 @@ Public Class Form1
                     ConsoleCommand(RegionClass.ProcessID(Y), "alert CPU Intensive Backup Started{ENTER}")
                     ConsoleCommand(RegionClass.ProcessID(Y), "change region " + """" + chosen + """" + "{ENTER}")
                     ConsoleCommand(RegionClass.ProcessID(Y), "save oar " + """" + BackupPath() + RegionClass.RegionName(Y) + "_" + DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss") + ".oar" + """" + "{Enter}")
-                    Application.DoEvents()
                 Next
             End If
             Me.Focus()
@@ -3824,7 +3858,7 @@ Public Class Form1
                         ConsoleCommand(RegionClass.ProcessID(Y), "alert New content Is loading..{ENTER}")
                         ConsoleCommand(RegionClass.ProcessID(Y), "load oar --force-terrain --force-parcels " + """" + thing + """" + "{ENTER}")
                         ConsoleCommand(RegionClass.ProcessID(Y), "alert New content just loaded." + "{ENTER}")
-                        Application.DoEvents()
+
                     Next
                 End If
             End If
@@ -3884,7 +3918,7 @@ Public Class Form1
                     If Not L.Contains(RegionClass.RegionName(Y)) Then
                         ConsoleCommand(RegionClass.ProcessID(n), "change region " + """" + RegionClass.RegionName(Y) + """" + "{Enter}")
                         ConsoleCommand(RegionClass.ProcessID(n), "save oar  " + """" + BackupPath() + RegionClass.RegionName(Y) + "_" + DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss") + ".oar" + """" + "{Enter}")
-                        Application.DoEvents()
+
                         L.Add(RegionClass.RegionName(Y))
                     End If
                 Next
@@ -4027,33 +4061,32 @@ Public Class Form1
         If region.Length = 0 Then Return
 
         Dim backMeUp = MsgBox("Make a backup first?", vbYesNo, "Backup?")
-            Dim num = RegionClass.FindRegionByName(region)
-            Dim GroupName = RegionClass.GroupName(num)
-            Dim once As Boolean = False
-            For Each Y In RegionClass.RegionListByGroupNum(GroupName)
-                Try
-                    If Not once Then
-                        Print("Opensimulator will load " + thing + ". This may take some time.")
-                        thing = thing.Replace("\", "/")    ' because Opensim uses unix-like slashes, that's why
+        Dim num = RegionClass.FindRegionByName(region)
+        Dim GroupName = RegionClass.GroupName(num)
+        Dim once As Boolean = False
+        For Each Y In RegionClass.RegionListByGroupNum(GroupName)
+            Try
+                If Not once Then
+                    Print("Opensimulator will load " + thing + ". This may take some time.")
+                    thing = thing.Replace("\", "/")    ' because Opensim uses unix-like slashes, that's why
 
-                        ConsoleCommand(RegionClass.ProcessID(Y), "change region " + region + "{ENTER}")
-                        If backMeUp = vbYes Then
-                            ConsoleCommand(RegionClass.ProcessID(Y), "alert CPU Intensive Backup Started {ENTER}")
-                            ConsoleCommand(RegionClass.ProcessID(Y), "save oar " + """" + BackupPath() + "Backup_" + DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss") + ".oar" + """" + "{Enter}")
-                        End If
-                        ConsoleCommand(RegionClass.ProcessID(Y), "alert New content Is loading ...{ENTER}")
-                        ConsoleCommand(RegionClass.ProcessID(Y), "load oar --force-terrain --force-parcels " + """" + thing + """" + "{ENTER}")
-                        ConsoleCommand(RegionClass.ProcessID(Y), "alert New content just loaded. {ENTER}")
-                        Application.DoEvents()
-                        once = True
+                    ConsoleCommand(RegionClass.ProcessID(Y), "change region " + region + "{ENTER}")
+                    If backMeUp = vbYes Then
+                        ConsoleCommand(RegionClass.ProcessID(Y), "alert CPU Intensive Backup Started {ENTER}")
+                        ConsoleCommand(RegionClass.ProcessID(Y), "save oar " + """" + BackupPath() + "Backup_" + DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss") + ".oar" + """" + "{Enter}")
                     End If
+                    ConsoleCommand(RegionClass.ProcessID(Y), "alert New content Is loading ...{ENTER}")
+                    ConsoleCommand(RegionClass.ProcessID(Y), "load oar --force-terrain --force-parcels " + """" + thing + """" + "{ENTER}")
+                    ConsoleCommand(RegionClass.ProcessID(Y), "alert New content just loaded. {ENTER}")
+                    once = True
+                End If
 
-                Catch ex As Exception
-                    Log("Error: " + ex.Message)
-                End Try
-            Next
+            Catch ex As Exception
+                Log("Error: " + ex.Message)
+            End Try
+        Next
 
-            Me.Focus()
+        Me.Focus()
 
     End Sub
 
@@ -5339,7 +5372,7 @@ Public Class Form1
             If RegionClass.Booted(X) Then
                 ConsoleCommand(RegionClass.ProcessID(X), "set log level " + msg + "{ENTER}")
             End If
-            Application.DoEvents()
+
         Next
 
     End Sub
@@ -5464,7 +5497,7 @@ Public Class Form1
                     ConsoleCommand(RegionClass.ProcessID(X), "change region  " + RegionClass.RegionName(X) + "{ENTER}")
                     ConsoleCommand(RegionClass.ProcessID(X), "alert " + Message + "{ENTER}")
                 End If
-                Application.DoEvents()
+
             Next
             If HowManyAreOnline = 0 Then
                 Print("Nobody is online")
