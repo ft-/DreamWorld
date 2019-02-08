d45361ce6b306598e2a1456bfe3d6208d01e704d
Fred Beckhusen <fred@mitsi.com>
2019-02-08 14:02:16 -0600

    Robust BaseHostname=>BaseURL

--- Installer_Src/Setup DreamWorld/FormSetup.vb
+++ Installer_Src/Setup DreamWorld/FormSetup.vb
@@ -990,7 +990,7 @@ Public Class Form1
 
         MySetting.SetOtherIni("DatabaseService", "ConnectionString", ConnectionString)
         MySetting.SetOtherIni("Const", "GridName", MySetting.SimName)
-        MySetting.SetOtherIni("Const", "BaseHostname", MySetting.PublicIP)
+        MySetting.SetOtherIni("Const", "BaseURL", MySetting.PublicIP)
         MySetting.SetOtherIni("Const", "PrivURL", "http://" + MySetting.PrivateURL)
         MySetting.SetOtherIni("Const", "PublicPort", MySetting.HttpPort) ' 8002
         MySetting.SetOtherIni("Const", "PrivatePort", MySetting.PrivatePort)
